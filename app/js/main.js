a4dc484b67119a42c60e635d6fca0a9ac280f31b
Kirill V <kirilliuswest@gmail.com>
2019-10-16 12:51:31 -0400

    removed loop in the main function for the if statement. added logic to remove the old question and proceed to the new one

--- app/js/main.js
+++ app/js/main.js
@@ -11,15 +11,7 @@ function getQuestions () {
 
 }
 // a function to control the count down
-function countDown () {
-    if (time <= 0) {
-        window.clearInterval(timerFunction);
-    }
-    else {
-        time--;
-        $("#timer").text("Time: "+time);
-    }
-}
+
 
 var time = 30;
 quizQuestions = questions;
@@ -29,33 +21,62 @@ function launchQuiz () {
     $("#landing").attr("hidden", true);
     // then, start countdown, display questions one after another and get answers
     var timerFunction = window.setInterval(countDown, 1000);
+    function countDown () {
+        if (time <= 0) {
+            window.clearInterval(timerFunction);
+        }
+        else {
+            time--;
+            $("#timer").text("Time: "+time);
+        }
+    }
+    // show questions and receive answers, until all questions are done
     i = 0;
-    while (i <= (quizQuestions.length - 1)) {
+    runQuiz (i);
+}
+
+
+function runQuiz (i) {
+    if (i <= quizQuestions.length - 1) {
         question = quizQuestions[i];
-        showQuestion(question);
-        i++;
+        hideQuestion(i-1);
+        showQuestion(question, i);
+        var answer = "";
+        $(".answer").on("click", function () {
+            answer = ($(this).attr("data"));
+            answer === question.answer ? window.alert("TRUE") : window.alert("FALSE");
+            i++;
+            runQuiz(i);
+            }
+        )
     }
 }
-    
 
 
 // function to display question on the screen as title with options
-function showQuestion (question) {
+function showQuestion (question, i) {
+    var container = $("<div>");
+    container.attr("class","container mt-5 row justify-content-center"); 
+    container.attr("id","question"+i); 
     var qBlock = $("<div>");
-    qBlock.attr("class","container mt-5");
+    qBlock.attr("class","mt-5 col-sm-6 offset-sm-3");
+    container.append(qBlock);
     var qTitle = $("<div>"+question.title+"</div>");
     qBlock.append(qTitle);
     for (i in question.choices) {
         choice = (parseInt(i)+1)+". "+question.choices[i];
         var qChoice = $("<button>"+choice+"</button><br>");
-        qChoice.attr("class","btn btn-primary mt-1");
+        qChoice.attr("class","btn btn-primary mt-1 answer");
         qChoice.attr("id", "answer"+i);
+        qChoice.attr("data", question.choices[i]);
         qBlock.append(qChoice);
     }
-    $("body").append(qBlock);
+    $("body").append(container);
 }
 
-
+function hideQuestion (i) {
+    $("#question"+i).remove();
+}
 
 
 
